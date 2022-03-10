2d5b77bc6e4bad7bb2459227895311ea34b041c9
nadiasabrina <nadia.sabrina91@ui.ac.id>
2022-03-10 12:35:01 +0700

    feat: ui login register

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,67 @@
-<!DOCTYPE html>
+
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SIPeMP Psikologi UI </title>
-</head>
-<body>
-    <h1>SELAMAT DATANG di SIPeMP Psikologi UI !!</h1>
-    <p>Sistem Informasi Penerimaan Program Magister Profesi <br> Fakultas Psikologi UI</p>
-    <img src="img/logo.png" alt=5>
-</body>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>SIPeMP Psikologi UI</title>
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/navbar-fixed/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{asset('front/css/bootstrap.min.css')}}" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="navbar-top-fixed.css" rel="stylesheet">
+  </head>
+  <body>
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"> <img src="img/logo.png" alt=5  width="205" height="82"> </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="margin-left:200px" class="mx-auto" >
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Pengumuman</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Admin</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Wawancara</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Pendaftar</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    <div style="margin-right:20px">
+        <a type="button" href="/login" class="btn btn-primary" style="background-color:#E48700; border:0px; width:80px">Masuk</a>
+    </div>
+    </nav>
+    <div style="width:1440px;border:2px solid #C9C1C1;"></div>
+
+    
+
+    <footer class="footer" style="position:fixed; bottom:0">
+      <div style="width:1440px; height: 100px; background-color:#58CCF4; padding:10px; display:flex">
+      <img src="img/logo 2.png" alt=5  width="205" height="82">
+      <div style="text-align:right"> 
+         
+        </div>
+      
+      </div>
+    </footer>
+
+        
+  </body>
 </html>
