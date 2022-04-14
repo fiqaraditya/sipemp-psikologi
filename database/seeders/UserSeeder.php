6be116dd18e8c07b2c67c22bff7d882b2bd3ebf6
dzul.fiqar <dzul.fiqar@ui.ac.id>
2022-04-14 19:10:10 +0700

    feat: seeder user Admin Master

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin Master',
+            'email' => 'adminmaster@email.com',
+            'role' => 'admin',
+            'password' => Hash::make('adminmasterpass'),
+        ]);
+    }
+}
